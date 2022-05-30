60c46e25297e6cf7a4657e5feb1f38eb9a1feb37
“Anoop <anoop.chauhan@plivo.com>
2022-05-30 15:36:50 +0530

    NEW: expense filter

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,25 +1,30 @@
 import './Expenses.css';
+
 import ExpenseItem from './ExpenseItem';
+import ExpenseFilter from './ExpenseFilter';
 import Card from '../UI/Card';
 
 const Expenses = ({ item }) => {
     return (
-        <Card className="expenses">
-            <ExpenseItem
-                title={item[0].title}
-                amount={item[0].amount}
-                date={item[0].date}
-            />
-            <ExpenseItem
-                title={item[1].title}
-                amount={item[1].amount}
-                date={item[1].date}
-            />
-            <ExpenseItem
-                title={item[2].title}
-                amount={item[2].amount}
-                date={item[2].date}
-            />
+        <Card className='expenses'>
+            <ExpenseFilter />
+            <div>
+                <ExpenseItem
+                    title={item[0].title}
+                    amount={item[0].amount}
+                    date={item[0].date}
+                />
+                <ExpenseItem
+                    title={item[1].title}
+                    amount={item[1].amount}
+                    date={item[1].date}
+                />
+                <ExpenseItem
+                    title={item[2].title}
+                    amount={item[2].amount}
+                    date={item[2].date}
+                />
+            </div>
         </Card>
     )
 }
